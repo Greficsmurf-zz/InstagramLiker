03bba17d2402ae017807a2494189d4faa3e85cff
greficsmurf <greficsmurf@gmail.com>
2018-02-03 21:31:45 +0500

    like all followers

--- index.js
+++ index.js
@@ -8,12 +8,33 @@ var driver = new webdriver.Builder().forBrowser('firefox').build();
 //'use strict'
 //login into account
  var login = async function(){
+ 	var arr = [];
  	var element;
  	var prom = new Promise(function(resolve, reject){
-         resolve(() => {
-            console.log("promise resolved");  	
-         });
+ 	//	if(config.Settings.all != 'true') resolve(() => console.log('resolvedByOne\n')); 
 
+         resolve(() => {
+         	driver.wait(webdriver.until.elementsLocated(webdriver.By.className('_si7dy')))
+         	.then(() => {
+         driver.navigate().to('https://www.instagram.com/timur_green/')
+         .then(() => {
+         	driver.wait(webdriver.until.elementsLocated(webdriver.By.className('_t98z6')))
+         	.then(() => driver.findElements(webdriver.By.className('_t98z6')))
+         	.then(found => found[2].click()); 
+         });
+     });
+        driver.wait(webdriver.until.elementsLocated(webdriver.By.className('_2g7d5 notranslate _o5iw8')))
+        .then(() => { 
+        	driver.findElements(webdriver.By.className('_2g7d5 notranslate _o5iw8'))
+        	.then(found => {
+                  for(var i = 0, j = 0; i < found.length; i++){
+                  	found[i].getAttribute('title')
+                  	.then(found => arr.push(found));
+                  }          
+        	});
+           });  	
+         });
+       return arr;
  	});
  	var classname = '_3jvtb';
     await driver.get('https://www.instagram.com/accounts/login/')
@@ -23,11 +44,13 @@ var driver = new webdriver.Builder().forBrowser('firefox').build();
     element = await driver.findElement(webdriver.By.name('password'));
     await element.sendKeys(config.Settings.password, webdriver.Key.RETURN);
     element = await driver.findElements(webdriver.By.className(classname));
-
+    
     return prom;
 };
 
-var like = async function(){
+
+var like = async function(arr){
+	console.log(arr[0]);
    var classname = '_mck9w _gvoze _f2mse';
    var heartClass = '_8scx2 coreSpriteHeartOpen', unheartClass = '_8scx2 coreSpriteHeartFull';
    var i = 0, text;
@@ -35,23 +58,23 @@ var like = async function(){
    await driver.sleep(1000);
    await driver.navigate().to('https://www.instagram.com/' + config.Settings.target + '/');
  
-   await driver.wait(webdriver.until.elementsLocated(webdriver.By.className(classname)));
+   driver.wait(webdriver.until.elementsLocated(webdriver.By.className(classname)));
+
+   await driver.executeScript('window.open("new")');
+   test = await driver.getAllWindowHandles();
+
    while(i != element.length){
    element = await driver.findElements(webdriver.By.tagName('a'));  
    text = await element[i++].getAttribute('href');
     
-    	await driver.actions().keyDown(webdriver.Key.SPACE).keyUp(webdriver.Key.SPACE).perform();
+    await driver.actions().keyDown(webdriver.Key.SPACE).keyUp(webdriver.Key.SPACE).perform();
      
    if(text.indexOf('taken-by=') != -1){
-   	//Strange momnent need to fix  {
 
-     await driver.executeScript('window.open("new")');
-     test = await driver.getAllWindowHandles();
      await driver.switchTo().window(test[1]);
      await driver.navigate().to(text);
 
      await driver.wait(webdriver.until.elementsLocated(webdriver.By.className('_rewi8')));
-   //  await driver.navigate().to(text);
     
      driver.findElements(webdriver.By.className(heartClass))
      .then(found => found[0].click());
@@ -63,6 +86,7 @@ var like = async function(){
 
    }
    }
+   await driver.quit();
    console.log('END');
    };
 
@@ -71,12 +95,12 @@ var like = async function(){
 try{
 
  login().then(found => {
-     found();
-     like();
+     like(found());
   });
  }catch(err){
  	console.log('ERROR ', err.message);
  }
+
 
 
 //Select an image
